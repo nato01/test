56e8809341ff685c8e56aa26b4df639fa3f285d3
nato01 <sibahu0104@yahoo.co.jp>
2019-09-20 15:00:14 +0900

    コメント

---
+++ Person/src/Test.java
@@ -0,0 +1,41 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+	  Person taro=new Person();
+	  taro.name="山田太郎";
+      taro.age=20;
+      System.out.println(taro.name);
+      System.out.println(taro.age);
+      taro.talk();
+
+      Person kimura=new Person();
+      kimura.name="木村次郎";
+      kimura.age=18;
+      System.out.println(kimura.name);
+      System.out.println(kimura.age);
+
+      Person suzuki=new Person();
+      suzuki.name="鈴木花子";
+      suzuki.age=16;
+      System.out.println(suzuki.name);
+      System.out.println(suzuki.age);
+
+      Person sakakibara=new Person();
+      sakakibara.name="榊原孝";
+      sakakibara.age=20;
+      sakakibara.phoneNumber="000-1111-2222";
+      sakakibara.address="aaa@yahoo.co.jp";
+      System.out.println(sakakibara.name);
+      System.out.println(sakakibara.age);
+      System.out.println(sakakibara.phoneNumber);
+      System.out.println(sakakibara.address);
+
+      Person aibo=new Person();
+      aibo.talk();
+
+      Person asimo=new P
+
+	}
+
+}
---
+++ TstProject01/src/Halloworld.java
@@ -0,0 +1,9 @@
+
+public class Halloworld {
+
+	public static void main(String[] args) {
+		// Hello World!の出力
+System.out.print("Hello World!");
+	}
+
+}
